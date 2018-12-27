74843227fb8b07f3cfe5b5eab2be06d4907ea639
Max Mearsheimer <max@seemaxwork.com>
2018-12-27 13:57:20 -0600

    redone locally

--- themes/FourSightHealth/partials/_ask-dave-popup.php
+++ themes/FourSightHealth/partials/_ask-dave-popup.php
@@ -1,7 +1,7 @@
 <div class="module-popup-card ask-dave-popup-card drop-shadow askDaveCard">
-  <h2>Make Me an ACF  Ask Dave</h2>
+  <h2>Ask Dave</h2>
   <h5>
-    Make Me an ACF Got a question about the issues we cover? Contact Dave and get the answers.
+    Got a question about the issues we cover? Contact Dave and get the answers.
   </h5>
   <div class="dave-popup-card-contacts">
     <div class="popup-contact-line">
